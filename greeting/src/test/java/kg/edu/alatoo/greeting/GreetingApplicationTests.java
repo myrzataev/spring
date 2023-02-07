363f16ee2bcb0e6636e39146b6b5a72701cb3fd3
myrzataev <99857663+myrzataev@users.noreply.github.com>
2023-02-07 11:08:02 +0600

    Add files via upload

---
+++ greeting/src/test/java/kg/edu/alatoo/greeting/GreetingApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.edu.alatoo.greeting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
